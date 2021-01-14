bf3156570eeac7006decdc3258b578994ced4d6b
Yuya-Oda <yo.jobhunting@gmail.com>
2021-01-14 18:48:51 +0900

    init()/初期化パラメーターの設定

--- src/servlet/CounterServlet_before.java
+++ src/servlet/CounterServlet_before.java
@@ -7,32 +7,23 @@ import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  * Servlet implementation class CounterServlet
  */
-// 初期化パラメーターの設定
-@WebServlet(
-		urlPatterns = {"/CounterServlet"},
-		initParams = {
-				@WebInitParam( name = "bgcolor", value = "yellow" )
-		})
-
-public class CounterServlet extends HttpServlet {
+@WebServlet("/CounterServlet_before")
+public class CounterServlet_before extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	String bgcolor;	//宣言しておかないと44行目でエラーが出る
-					// ★ 89行目とリンク
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
-	//public void init() throws ServletException {
 		/*
 		 * ① 何らかのパラメーターを受け取って初期化処理をおこなう場合
 		 * → パラメータありのinitメソッド
@@ -41,22 +32,15 @@ public class CounterServlet extends HttpServlet {
 		 * → パラメータなしのinitメソッド
 		 */
 		super.init(config); // 制約あり（記述必須：教科書P.315）
-		//		super.init();		 // 記述の制約なし
-
-		try {
-			bgcolor = config.getInitParameter("bgcolor");
-			System.out.println("bgcolor:" + bgcolor);
-		}catch(Exception e) {
-			System.out.println("error:" + e.toString());
-		}
+//		super.init();		 // 記述の制約なし
 
 		// 訪問回数を表すIntegerインスタンスを新規作成しアプリケーションスコープに保存
 		Integer count = 0;
 
 		ServletContext application = config.getServletContext();
-		//										↑ 引数のインスタンスでもOK
-		//										↓ 自身のインスタンスでもOK
-		//		ServletContext application = this.getServletContext();
+//										↑ 引数のインスタンスでもOK
+//										↓ 自身のインスタンスでもOK
+//		ServletContext application = this.getServletContext();
 
 		application.setAttribute("count", count);
 
@@ -68,11 +52,11 @@ public class CounterServlet extends HttpServlet {
 	 * @see Servlet#destroy()
 	 */
 
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// アプリケーションスコープに保存された訪問回数を増加
 		ServletContext application = this.getServletContext();
 
@@ -85,11 +69,7 @@ public class CounterServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<head>");
-		out.println("<meta charset=\"UTF-8\" />");
 		out.println("<title>訪問回数を表示</title>");
-		out.println("<style>");
-		out.println("body{background: " + bgcolor + ";}"); // ★
-		out.println("</style>");
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<p>訪問回数：" + count + "</p>");
@@ -101,8 +81,7 @@ public class CounterServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
